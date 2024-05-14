43ca9d44d3f66673ae9aa7b5060f7068e7fbb899
Tứ Trần Lê <21522746@ms.uit.edu.vn>
2024-05-14 09:12:56 +0700

    goomba

---
+++ Box.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_BOX 61000
+#define BOX_WIDTH 16
+#define BOX_BBOX_WIDTH 16
+#define BOX_BBOX_HEIGHT 16
+
+class CBox : public CGameObject {
+public:
+	CBox(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
